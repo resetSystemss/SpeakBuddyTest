e81758c654a60d6b5cadb542dc874012d721774c
Reset Systems <62904767+resetSystemss@users.noreply.github.com>
2023-12-11 22:43:17 -0500

    First commit

---
+++ SpeakBuddyJoseGonzalez/SpeakBuddyJoseGonzalez/SpeakBuddyJoseGonzalezApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakBuddyJoseGonzalezApp.swift
+//  SpeakBuddyJoseGonzalez
+//
+//  Created by José González on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeakBuddyJoseGonzalezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
